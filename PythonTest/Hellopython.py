6797bad5dc28eaa7c8dd93e8456051888923a600
Hiroki Ueda <u2000hiroki@gmail.com>
2019-12-25 21:05:23 +0900

    test

--- PythonTest/Hellopython.py
+++ PythonTest/Hellopython.py
@@ -4,3 +4,10 @@ print(3.14)
 print("""改行
       できる""")
 print("数字は",10,"カンマで区切って表示できる")
+print("改行は\nでもできる")
+print("pythonは動的型言語")
+test="変数のtest"
+print(test,"こういう風に変数を使える")
+print("1+4=",1+4," 6*7=",6*7," 5**5=",5**5)
+inputtest=input("値を入力できる")
+print(inputtest,"が入力された")
